b21515b3aeb11ae8ef190b2f0a99c91d73f9f260
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2023-01-26 20:17:39 +0800

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -5,17 +5,17 @@ ckpt=deit_small_patch16_224-cd65a155.pth
 
 echo "output dir: $logdir"
 
-python3 -m torch.distributed.launch --nproc_per_node=8 --use_env \
+python3 -m torch.distributed.launch --nproc_per_node=2 --use_env \
 	main.py \
 	--model deit_small_patch16_shrink_base \
 	--fuse_token \
-	--base_keep_rate 0.7 \
+	--base_keep_rate 0.3 \
 	--input-size 224 \
 	--sched cosine \
 	--lr 2e-5 \
 	--min-lr 2e-6 \
 	--weight-decay 1e-6 \
-	--batch-size 256 \
+	--batch-size 512 \
 	--shrink_start_epoch 0 \
 	--warmup-epochs 0 \
 	--shrink_epochs 0 \
