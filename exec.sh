e323e1c3e5fd179fa6a7949271c361719d903c86
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2023-01-28 09:23:57 +1000

    updated

--- exec.sh
+++ exec.sh
@@ -3,9 +3,9 @@
 #SBATCH -n 1
 #SBATCH --job-name=evit0.3
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=50
-#SBATCH --gres=gpu:tesla-smx2:2
-#SBATCH --mem-per-cpu=2G
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=1G
 #SBATCH -o EViT0.3_out.txt
 #SBATCH -e EViT0.3_err.txt
 
